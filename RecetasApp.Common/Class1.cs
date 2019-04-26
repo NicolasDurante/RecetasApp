33b186ad5b62fe152b542296c40221e462a03281
NicolásDurante <fer-nicolas-durante@hotmail.com>
2019-04-25 21:22:48 -0300

    Creacion del proyecto

---
+++ RecetasApp.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RecetasApp.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ RecetasApp.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RecetasApp.UIClassic
+{
+    public class Class1
+    {
+    }
+}
