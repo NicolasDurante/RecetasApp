bde3a67ca2d62463cf6d666e9b3250d32efcf459
NicolásDurante <fer-nicolas-durante@hotmail.com>
2019-04-29 18:48:12 -0300

    Api Recetas Controller

---
+++ RecetasApp.Web/Controllers/API/RecetasController.cs
@@ -0,0 +1,22 @@
+﻿namespace RecetasApp.Web.Controllers.API
+{
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+    public class RecetasController : Controller
+    {
+        private readonly IRecetaRepository recetaRepository;
+
+        public RecetasController(IRecetaRepository recetaRepository)
+        {
+            this.recetaRepository = recetaRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetRecetas()
+        {
+            return Ok(this.recetaRepository.GetAll());
+        }
+    }
+}
