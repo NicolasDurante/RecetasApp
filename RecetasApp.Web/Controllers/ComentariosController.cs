bc34f481bdab266eafe90dc8fbb61b9f6c071490
NicolásDurante <fer-nicolas-durante@hotmail.com>
2019-12-09 01:49:48 -0300

    Todas las interfaces web terminadas

---
+++ RecetasApp.Web/Controllers/ComentariosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RecetasApp.Web.Data;
+using RecetasApp.Web.Data.Entities;
+
+namespace RecetasApp.Web.Controllers
+{
+    public class ComentariosController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ComentariosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Comentarios
+        public async Task<IActionResult> Index()
+        {
+            var dataContext = _context.Comentarios.Include(c => c.Receta);
+            return View(await dataContext.ToListAsync());
+        }
+
+        // GET: Comentarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentario = await _context.Comentarios
+                .Include(c => c.Receta)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentario);
+        }
+
+        // GET: Comentarios/Create
+        public IActionResult Create()
+        {
+            ViewData["RecetaId"] = new SelectList(_context.Recetas, "Id", "Descripcion");
+            return View();
+        }
+
+        // POST: Comentarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,RecetaId,Comentari")] Comentario comentario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(comentario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RecetaId"] = new SelectList(_context.Recetas, "Id", "Descripcion", comentario.RecetaId);
+            return View(comentario);
+        }
+
+        // GET: Comentarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentario = await _context.Comentarios.FindAsync(id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+            ViewData["RecetaId"] = new SelectList(_context.Recetas, "Id", "Descripcion", comentario.RecetaId);
+            return View(comentario);
+        }
+
+        // POST: Comentarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,RecetaId,Comentari")] Comentario comentario)
+        {
+            if (id != comentario.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comentario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ComentarioExists(comentario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RecetaId"] = new SelectList(_context.Recetas, "Id", "Descripcion", comentario.RecetaId);
+            return View(comentario);
+        }
+
+        // GET: Comentarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentario = await _context.Comentarios
+                .Include(c => c.Receta)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentario);
+        }
+
+        // POST: Comentarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var comentario = await _context.Comentarios.FindAsync(id);
+            _context.Comentarios.Remove(comentario);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ComentarioExists(int id)
+        {
+            return _context.Comentarios.Any(e => e.Id == id);
+        }
+    }
+}
